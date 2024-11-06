c13a98e113006374bf2872939f3131857149dd4c
VincenzoIuliano <bubucane@outlook.it>
2024-11-06 12:18:09 +0100

    ADD: create sample card

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 console.log('ciao')
+
+
+// <div class="col-md-6 col-lg-4 col-xl-3">
+// <div class="card">
+//   <div class="card-body">
+//     <h5 class="card-title">Card title</h5>
+//     <p class="card-text">
+//       Some quick example text to build on the card title and make
+//       up the bulk of the card's content.
+//     </p>
+//     <a href="#" class="card-link">Card link</a>
+//   </div>
+// </div>
+// </div>
